470598a9fb47c2a3d4bde850176a877c22780e0b
Cxndr <livewellandmakethings@gmail.com>
2025-02-12 13:02:18 +0000

    fightload change to correct getEnemyParty call

--- src/components/FightLoad.tsx
+++ src/components/FightLoad.tsx
@@ -18,7 +18,7 @@ export default function FightLoad({game, setGame, setMonSelection}: FightLoadPro
     let enemyCount = 3;
     if (game.round === 1) enemyCount = 1;
     if (game.round === 2) enemyCount = 2;
-    const enemyParty = await getEnemyParty(game.round*5, enemyCount);
+    const enemyParty = await getEnemyParty(game.round*5, enemyCount, game.round);
     setEnemyParty(enemyParty);
     setEnemyLoaded(true);
     setMonSelection(enemyParty);
